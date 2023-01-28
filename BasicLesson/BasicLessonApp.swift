d1997526a44a66c37a72cb7f621b55481d93ca07
yohei shimizu <yohei.shimizu.1190@gmail.com>
2023-01-28 20:49:59 +0900

    Initial Commit

---
+++ BasicLesson/BasicLessonApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicLessonApp.swift
+//  BasicLesson
+//
+//  Created by yohei shimizu on 2023/01/28.
+//
+
+import SwiftUI
+
+@main
+struct BasicLessonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
