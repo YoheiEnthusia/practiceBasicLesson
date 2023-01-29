c354ae3fb8249310053fb3ffafb5c76822227854
yohei shimizu <yohei.shimizu.1190@gmail.com>
2023-01-29 16:22:41 +0900

    アニメーションを追加2

--- BasicLesson/ContentView.swift
+++ BasicLesson/ContentView.swift
@@ -23,7 +23,7 @@ extension Image {
 }
 
 struct ContentView: View {
-    private let imageURL: String = "https://credo.academy/credo-academy@3x.pn"
+    private let imageURL: String = "https://credo.academy/credo-academy@3x.png"
     
     
     var body: some View {
@@ -42,6 +42,7 @@ struct ContentView: View {
         */
         
         // MARK: - フェーズを使用して、エラーハンドリング
+        /*
         AsyncImage(url: URL(string: imageURL)) { phase in
             if let image = phase.image {
                 image.imageModifier()
@@ -51,6 +52,24 @@ struct ContentView: View {
                 Image(systemName: "photo.circle.fill").iconModifier()
             }
         }.padding(40)
+        */
+        
+        // MARK: - アニメーションを追加
+        AsyncImage(url: URL(string: imageURL), transaction: Transaction(animation: .spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0.25))) { phase in
+            switch phase {
+            case .success(let image) :
+                image
+                    .imageModifier()
+                    .transition(.scale)
+            case .failure(_) :
+                Image(systemName: "ant.circle.fill").iconModifier()
+            case .empty :
+                Image(systemName: "photo.circle.fill").iconModifier()
+            @unknown default:
+                ProgressView()
+            }
+        }
+        .padding(40)
     }
 }
 
