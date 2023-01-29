0a76caacc3582991ac1139f68bae8cb161cb255d
yohei shimizu <yohei.shimizu.1190@gmail.com>
2023-01-29 15:07:11 +0900

    画像の非同期処理を追加

--- BasicLesson/ContentView.swift
+++ BasicLesson/ContentView.swift
@@ -7,15 +7,37 @@
 
 import SwiftUI
 
+extension Image {
+    func imageModifier() -> some View {
+        self
+            .resizable()
+            .scaledToFit()
+    }
+    
+    func iconModifier() -> some View {
+        self
+            .imageModifier()
+            .frame(maxWidth: 120)
+            .opacity(0.5)
+    }
+}
+
 struct ContentView: View {
+    private let imageURL: String = "https://credo.academy/credo-academy@3x.png"
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        // MARK: - 1. スケール修正。scaleが大きいほど縮小される
+//        AsyncImage(url: URL(string: imageURL), scale: 3.0)
+        
+        // MARK: - 2. プレースホルダー
+        AsyncImage(url: URL(string: imageURL)) { image in
+            image.imageModifier()
+        } placeholder: {
+            Image(systemName: "photo.circle.fill").iconModifier()
         }
-        .padding()
+        .padding(40)
+
     }
 }
 
